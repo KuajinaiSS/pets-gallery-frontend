27682698253246ae53cb603273045c0a566c0dc4
KuajinaiSS <kuajinaiss@gmail.com>
2025-10-21 01:14:03 -0300

    Refactor project structure and integrate Tailwind CSS
    - Removed PostCSS configuration file.
    - Updated layout to use global CSS instead of PrimeReact styles.
    - Added new Admin, Login, and Register pages with basic structure.
    - Created a User page utilizing Flowbite components.
    - Introduced global CSS file for Tailwind CSS and Flowbite integration.
    - Configured Tailwind CSS with content paths for proper purging and plugin support.

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Login() {
+    return (
+        <div className="card">
+            <p>Login Page</p>
+        </div>
+    );
+}
---
+++ src/app/user/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Button } from "flowbite-react";
+
+export default function User() {
+    return (
+        <main className="min-h-screen flex items-center justify-center gap-6 p-8">
+            <Button color="purple">Default</Button>
+        </main>
+    );
+}
